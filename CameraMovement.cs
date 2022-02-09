8d6941f2f30c439497e6f478cf232c8810a523c1
Thirdplace Studios <thirdplace.studios.dev@gmail.com>
2022-02-09 14:41:08 -0500

    Update CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,34 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
 
-    public GameObject player;
-    public GameObject camera;
+    public Transform player;
+    public Transform camera;
 
     public float xOffset = 0;
 
     float xCamera;
-    float yCamera;
     float xPlayer;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
-        xCamera = camera.transform.position.x;
-        yCamera = camera.transform.position.y;
-        xPlayer = player.transform.position.x;
+        xCamera = camera.position.x;
+        xPlayer = player.position.x;
 
-        if(xPlayer>xCamera-xOffset)
+        if (xPlayer > xCamera - xOffset)
         {
-            camera.transform.position = new Vector2(xPlayer + xOffset, yCamera);
-        } else
-        {
-            camera.transform.position = new Vector2(xCamera, yCamera);
+            camera.position = new Vector2(xPlayer + xOffset, camera.position.y);
         }
-        
+        else
+        {
+            camera.position = new Vector2(xCamera, camera.position.y);
+        }
+
     }
 }
