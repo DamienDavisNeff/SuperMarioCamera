6bd672b72c5991c4434dd8a2a8637c7d6e318515
Thirdplace Studios <thirdplace.studios.dev@gmail.com>
2022-05-21 18:16:54 -0400

    Updated CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,30 +1,36 @@
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class CameraMovement : MonoBehaviour
 {
+    Transform t_Camera; // Camera Controller (To Move)
+    Camera c_Camera; // Camera (For Size)
+    Transform t_Player; // Player (For Position)
+    BoxCollider2D c_Barrier; // Collider (To Keep Player From Exiting)
+    private float x_Camera; // Position Of The Camera
 
-    public Transform player;
-    public Transform camera;
+    [Header("Camera Options")]
+    [SerializeField][Tooltip("The Offset On The X Axis")]
+    private float x_Offset = 0f; // How Much The Camera Should Be Offset On The X Axis
 
-    public float xOffset = 0;
-
-    float xCamera;
-    float xPlayer;
-
-    void FixedUpdate()
-    {
-
-        xCamera = camera.position.x;
-        xPlayer = player.position.x;
-
-        if (xPlayer > xCamera - xOffset)
-        {
-            camera.position = new Vector2(xPlayer + xOffset, camera.position.y);
-        }
-        else
-        {
-            camera.position = new Vector2(xCamera, camera.position.y);
-        }
-
+    void Start() {
+      t_Camera = this.transform; // Sets Camera Controller
+      c_Camera = Camera.main; // Sets Camera
+      t_Player = GameObject.FindGameObjectsWithTag("Player")[0].transform; // Sets Player
+      c_Barrier = this.GetComponent<BoxCollider2D>(); // Sets Collider
     }
+
+    void Update() {
+      CameraStuff(); // Finds Camera Position & Barriers
+      float x_Player = t_Player.position.x; // Finds Player Position
+      if(x_Player > x_Camera - x_Offset) t_Camera.position = new Vector2(x_Player + x_Offset, t_Camera.position.y); // Moves Camera
+    }
+
+    void CameraStuff() {
+      x_Camera = t_Camera.position.x; // Finds Camera Position
+      Vector2 v_CameraSize = c_Camera.ScreenToWorldPoint(new Vector2(c_Camera.pixelWidth,c_Camera.pixelHeight)); // Finds Camera Size In World Space
+      c_Barrier.size = new Vector2(0.1f,v_CameraSize.y * 2); // Sets Height Of Barrier To Height Of Camera
+      c_Barrier.offset = new Vector2(-v_CameraSize.x,0); // Sets Barrier To Left Side Of Camera
+    }
+
 }
