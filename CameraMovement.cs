c3d30243422354a43c03fa96b1f5aafedcf676c9
Damien Davis-Neff <damiendavisneff@gmail.com>
2022-08-06 20:57:24 -0400

    smoothed camera movement

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,34 +1,31 @@
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class CameraMover : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
-    Transform t_Camera; // Camera Controller (To Move)
-    Camera c_Camera; // Camera (For Size)
-    Transform t_Player; // Player (For Position)
-    BoxCollider2D c_Barrier; // Collider (To Keep Player From Exiting)
-
+    Transform t_Camera; // Camera Controller
+    Camera c_Camera; // Camera
+    Transform t_Player; // Player
+    BoxCollider2D c_Barrier; // Collider
     [Header("Camera Options")]
     [SerializeField][Tooltip("The Offset On The X Axis")]
-    private float x_Offset = 0f; // How Much The Camera Should Be Offset On The X Axis
+    private float x_Offset = 0f;
+    [SerializeField][Tooltip("The Scale Of Camera Smoothing")]
+    private float cameraScale = 1f;
 
-    void Start() {
-      t_Camera = this.transform; // Sets Camera Controller
-      c_Camera = Camera.main; // Sets Camera
-      t_Player = GameObject.FindGameObjectsWithTag("Player")[0].transform; // Sets Player
-      c_Barrier = this.GetComponent<BoxCollider2D>(); // Sets Collider
-
-      Vector2 v_CameraSize = c_Camera.ScreenToWorldPoint(new Vector2(c_Camera.pixelWidth,c_Camera.pixelHeight)); // Finds Camera Size In World Space
-
-      c_Barrier.size = new Vector2(0.1f,v_CameraSize.y * 2); // Sets Height Of Barrier To Height Of Camera
-      c_Barrier.offset = new Vector2(-v_CameraSize.x,0); // Sets Barrier To Left Side Of Camera
+    void Awake() {
+        t_Camera = this.transform;
+        c_Camera = Camera.main;
+        t_Player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        c_Barrier = this.GetComponent<BoxCollider2D>();
+        Vector2 v_CameraSize = c_Camera.ScreenToWorldPoint(new Vector2(c_Camera.pixelWidth,c_Camera.pixelHeight));
+        c_Barrier.size = new Vector2(0.1f,v_CameraSize.y * 2);
+        c_Barrier.offset = new Vector2(-v_CameraSize.x,0);
     }
 
-    void Update() {
-      float x_Camera = t_Camera.position.x; // Finds Camera Position
-      float x_Player = t_Player.position.x; // Finds Player Position
-      if(x_Player > x_Camera - x_Offset) t_Camera.position = new Vector3(x_Player + x_Offset, t_Camera.position.y, t_Camera.position.z); // Moves Camera
-
+    void FixedUpdate() {
+        float x_Camera = t_Camera.position.x;
+        float x_Player = t_Player.position.x;
+        if(x_Player > x_Camera - x_Offset) t_Camera.position = Vector3.Lerp(t_Camera.position, new Vector3(x_Player, t_Camera.position.y, -10), Time.fixedDeltaTime * cameraScale);
     }
-
 }
